a842c66047f6bc98221a0343c2e646dfe27e6c8b
casali1 <frank_casali@hotmail.com>
2019-07-10 17:56:33 -0600

    Add project files.

---
+++ DataAccessLibrary/EntityModels/ContactDBContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataAccessLibrary.EntityModels
+{
+    public partial class ContactDBContext : DbContext
+    {
+        public ContactDBContext()
+        {
+        }
+
+        public ContactDBContext(DbContextOptions<ContactDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contacts> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ContactDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Contacts>(entity =>
+            {
+                entity.HasKey(e => e.ContactId).HasName("PK_Contact");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+
+                entity.Property(e => e.Phone).HasMaxLength(50);
+            });
+        }
+    }
+}
